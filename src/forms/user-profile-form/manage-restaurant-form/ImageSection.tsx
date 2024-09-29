6d5461bc815dce891644a33988c3d6a968712e98
unknown <9nadsg@gmail.com>
2024-09-29 18:11:11 +0545

    adding user profile form image section

--- src/forms/user-profile-form/manage-restaurant-form/ImageSection.tsx
+++ src/forms/user-profile-form/manage-restaurant-form/ImageSection.tsx
@@ -1,10 +1,13 @@
 import { AspectRatio } from "@/components/ui/aspect-ratio";
-import { FormControl, FormDescription, FormField, FormItem } from "@/components/ui/form";
+import { 
+  FormControl, 
+  FormDescription, 
+  FormField, 
+  FormItem,
+  FormMessage,  
+ } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useFormContext } from "react-hook-form";
-
-
-
 
 const ImageSection = () => {
     const { control, watch } = useFormContext();
@@ -12,13 +15,14 @@ const ImageSection = () => {
     const existingImageUrl = watch("imageUrl");
 
   return (
-    <div>
+    <div className="space-y-2">
          <div>
             <h2 className="text-2xl font-bold">Add Image</h2>
             <FormDescription>
                 Add an image that will be displayed on your restaurant listing in the search results.Adding a new image will overwrite the existing one.
             </FormDescription>
          </div>
+
          <div className="flex flex-col gap-10 md:w-[50%]">
           {existingImageUrl && (
             <AspectRatio ratio={16/9}>
@@ -39,14 +43,17 @@ const ImageSection = () => {
                         className="bg-white" 
                         type="file" 
                         accept=".jpg, .jpeg, .png" 
-                        onChange={(event) => field.onChange(event.target.files ? event.target.files[0] : null)}
+                        onChange={(event) => 
+                          field.onChange(
+                            event.target.files ? event.target.files[0] : null
+                          )
+                        }
                       />
                     </FormControl>
+                    <FormMessage />
                   </FormItem>
-
                 )}
-            >
-            </FormField>
+            />
          </div>
     </div>
   )
